111fedb69f8f76e81bf2d201a2646059e66408f7
vopaldragon <takanori.k.nesi@gmail.com>
2023-02-11 14:07:44 +0900

    gpt3を使うAPIの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    top
-    <NuxtLink to="/test">Test</NuxtLink>
+    <textarea v-model="q"></textarea>
+    <textarea v-model="answer"></textarea>
+    <button @click="run()">RUN</button>
   </div>
 </template>
+
+<script setup lang="ts">
+const q = ref("")
+const answer = ref("")
+
+const run = async () => {
+  let a = await useFetch("/api/gpt3", {
+    method: "POST",
+    body: { input: q.value },
+  })
+  answer.value = a.data.value || ""
+}
+</script>
