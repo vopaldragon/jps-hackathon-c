f197a3eae1b6a4becb9795df2ad63d48bf3d57b1
Kazuki Imamura <kaizumaki@gmail.com>
2023-02-12 16:04:09 +0900

    （試験的）stable-diffussionのAPIを叩く実装を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      replicateApiURL: 'https://api.replicate.com/v1'
+    }
+  }
 })
