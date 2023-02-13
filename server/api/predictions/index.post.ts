bc05b1298528184721bd6bafae008b6740881e1d
Kazuki Imamura <kaizumaki@gmail.com>
2023-02-14 04:53:14 +0900

    コメントを修正

--- server/api/predictions/index.post.ts
+++ server/api/predictions/index.post.ts
@@ -10,8 +10,8 @@ export default defineEventHandler(async (event) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      // Pinned to a specific version of Stable Diffusion
-      // See https://replicate.com/stability-ai/stable-diffussion/versions
+      // Pinned to a specific version of instruct-pix2pix
+      // See https://replicate.com/timothybrooks/instruct-pix2pix/versions
       version: "30c1d0b916a6f8efce20493f5d61ee27491ab2a60437c13c588468b9810ec23f",
 
       // This is the text prompt that will be submitted by a form on the frontend
