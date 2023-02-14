af698d596693c3cafc791620606d9d1f10cbf51f
vopaldragon <takanori.k.nesi@gmail.com>
2023-02-14 22:42:07 +0900

    3クエリ選択の画面を作ってみた

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,367 @@
 <template>
   <div>
-    <form @submit.prevent="search()">
-      <input type="text" v-model="q" /><button type="submit">検索</button>
-    </form>
-
-    <div class="search-result" v-if="result">
-      <div class="search-item" v-for="item in result.list">
-        <img :src="item.common.thumbnailUrl[0]" />
-        <h3>{{ item.common.title }}</h3>
-        <p>{{ item.common.description }}</p>
+    <h1>
+      <img src="/title.png" alt="Fake Japan Search" />
+    </h1>
+    <p class="description">日本のあることないことを調べよう</p>
+    <div class="query">
+      <div class="words">
+        <h2>時代</h2>
+        <button
+          :class="{ selected: currentEra === era }"
+          v-for="era in eras"
+          @click="selectEra(era)"
+        >
+          {{ era }}
+        </button>
       </div>
+      <div class="cross" v-if="currentEra">×</div>
+      <div class="words" v-if="currentEra">
+        <h2>キーワード</h2>
+        <button
+          :class="{ selected: currentKeyword === keyword }"
+          v-for="keyword in keywords[currentEra]"
+          @click="selectKeyword(keyword)"
+        >
+          {{ keyword }}
+        </button>
+      </div>
+      <div class="cross" v-if="currentKeyword">×</div>
+      <div class="words" v-if="currentKeyword">
+        <h2>フェイク</h2>
+        <button
+          :class="{ selected: currentFake === fake }"
+          v-for="fake in fakes"
+          @click="selectFake(fake)"
+        >
+          {{ fake }}
+        </button>
+      </div>
+    </div>
+
+    <main>
+      <div v-if="loading" class="loader">Loading...</div>
+      <div class="search-result" v-if="result">
+        <div
+          class="search-item"
+          :class="{ fake: showFake && item.fake }"
+          v-for="item in result"
+        >
+          <img :src="item.thumb" />
+          <h3>{{ item.title }}</h3>
+          <p>
+            {{ description(item)
+            }}<button
+              v-if="item.description.length > 100"
+              class="show-more"
+              @click="item.show = !item.show"
+            >
+              {{ item.show ? "閉じる" : "もっと表示" }}
+            </button>
+          </p>
+        </div>
+      </div>
+    </main>
+    <div class="foot" v-show="result">
+      <label><input type="checkbox" v-model="showFake" />おみとおし</label>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { SearchResult } from "@/domain/item"
+const result = ref<
+  | {
+      title: string
+      description: string
+      fake: boolean
+      show: boolean
+      thumb: string
+    }[]
+  | null
+>(null)
 
-const q = ref("")
-const answer = ref("")
-const result = ref<SearchResult>()
+const eras = [
+  "縄文・弥生・古墳",
+  "飛鳥",
+  "奈良",
+  "平安",
+  "鎌倉",
+  "室町",
+  "安土桃山",
+  "江戸",
+  "明治",
+  "大正",
+  "昭和",
+  "平成",
+]
 
-const search = async () => {
-  result.value = (
-    await useFetch("/api/search", {
-      method: "POST",
-      body: { keyword: q.value },
-    })
-  ).data.value as any
+const currentEra = ref("")
+const currentKeyword = ref("")
+const currentFake = ref("")
+
+const keywords: { [key: string]: string[] } = {
+  "縄文・弥生・古墳": ["古墳", "埴輪", "縄文土器"],
+  飛鳥: ["A", "B", "C"],
 }
 
-const run = async () => {
-  let a = await useFetch("/api/gpt3", {
-    method: "POST",
-    body: { input: q.value },
-  })
-  answer.value = a.data.value || ""
+const fakes = ["シンプル", "トンデモ", "プレーン"]
+
+function selectEra(e: string) {
+  currentEra.value = e
+  currentKeyword.value = ""
 }
+
+function selectKeyword(e: string) {
+  currentKeyword.value = e
+  search()
+}
+
+function selectFake(e: string) {
+  currentFake.value = e
+  search()
+}
+
+const loading = ref(false)
+
+async function search() {
+  if (currentKeyword.value && currentFake.value) {
+    loading.value = true
+    result.value = null
+    try {
+      result.value = (
+        await useFetch("/api/search", {
+          method: "POST",
+          body: { keyword: currentKeyword.value, fake: currentFake.value },
+        })
+      ).data.value
+    } catch (e) {
+      alert(e)
+    } finally {
+      loading.value = false
+    }
+  }
+}
+
+function description(i: any) {
+  if (i.description.length > 100) {
+    if (i.show) {
+      return i.description
+    } else {
+      return i.description.substring(0, 100) + "..."
+    }
+  } else {
+    return i.description
+  }
+}
+
+const showFake = ref(false)
 </script>
 
 <style lang="scss">
-.search-result {
+$primary: #6941a0;
+h1 {
   display: flex;
-  flex-wrap: wrap;
-  .search-item {
-    width: 16rem;
+  justify-content: center;
+}
+
+.description {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+}
+
+.query {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+
+  .words {
+    display: flex;
+    gap: 1rem;
+    h2 {
+      font-size: 0.875rem;
+    }
+    button {
+      cursor: pointer;
+      background-color: transparent;
+      border: none;
+      padding: 0;
+      appearance: none;
+      background: none;
+      font-feature-settings: "liga";
+      -webkit-tap-highlight-color: transparent;
+
+      border-radius: 9999px;
+
+      cursor: pointer;
+      padding: 0.25rem 0.75rem;
+      font-size: 0.875rem;
+      line-height: 0.875rem;
+      text-align: center;
+      white-space: nowrap;
+
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      white-space: nowrap;
+
+      background-color: $primary;
+      color: white;
+      &:not(:disabled):hover {
+        background-color: darken($primary, 20%);
+        color: white;
+      }
+      &:hover {
+        transition: all 250ms;
+      }
+      &.selected {
+        background-color: adjust-hue($primary, 180deg);
+        &:hover {
+          background-color: adjust-hue($primary, 180deg);
+        }
+      }
+    }
   }
+
+  .cross {
+    font-size: 1.5rem;
+  }
+}
+
+main {
+  margin-top: 2rem;
+
+  .search-result {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+    .search-item {
+      width: 16rem;
+      img {
+        height: 256px;
+        object-fit: contain;
+      }
+      .show-more {
+        margin-left: 1rem;
+        cursor: pointer;
+        background-color: transparent;
+        border: none;
+        padding: 0;
+        appearance: none;
+        background: none;
+        font-feature-settings: "liga";
+        -webkit-tap-highlight-color: transparent;
+      }
+      &.fake {
+        border: 1px solid $primary;
+      }
+    }
+  }
+  .loader {
+    color: $primary;
+    font-size: 60px;
+    text-indent: -9999em;
+    overflow: hidden;
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    margin: 72px auto;
+    position: relative;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: load6 1.7s infinite ease, round 1.7s infinite ease;
+    animation: load6 1.7s infinite ease, round 1.7s infinite ease;
+  }
+  @-webkit-keyframes load6 {
+    0% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+    5%,
+    95% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+    10%,
+    59% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em,
+        -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em,
+        -0.297em -0.775em 0 -0.477em;
+    }
+    20% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+        -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em,
+        -0.749em -0.34em 0 -0.477em;
+    }
+    38% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+        -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em,
+        -0.82em -0.09em 0 -0.477em;
+    }
+    100% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+  }
+  @keyframes load6 {
+    0% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+    5%,
+    95% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+    10%,
+    59% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em,
+        -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em,
+        -0.297em -0.775em 0 -0.477em;
+    }
+    20% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+        -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em,
+        -0.749em -0.34em 0 -0.477em;
+    }
+    38% {
+      box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+        -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em,
+        -0.82em -0.09em 0 -0.477em;
+    }
+    100% {
+      box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+        0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+    }
+  }
+  @-webkit-keyframes round {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes round {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+}
+
+.foot {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- server/api/search.ts
+++ server/api/search.ts
@@ -1,6 +1,5 @@
-import { SearchResult } from "~~/domain/item"
-
 import { Configuration, OpenAIApi } from "openai"
+const config = useRuntimeConfig()
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_KEY,
@@ -20,25 +19,75 @@ const genGpt = async (prompt: string) => {
     return response.data.choices[0].text || ""
 }
 
+export type Item = {
+    title: string;
+    description: string;
+    fake: boolean;
+    thumb: string;
+}
 
-export default defineEventHandler(async (event): Promise<SearchResult> => {
-    const body = await readBody(event)
-    const response = await $fetch(`https://jpsearch.go.jp/api/item/search/jps-cross`, {
+
+export default defineEventHandler(async (event): Promise<Item[]> => {
+    const body: { keyword: string, fake: string } = await readBody(event)
+    console.info(body);
+    const response: any = await $fetch(`https://jpsearch.go.jp/api/item/search/jps-cross`, {
         method: 'get',
         query: {
             keyword: body.keyword,
             "f-contents": ["thumb"],
             exists: ["common.description"],
-            size: 10
+            size: 8
         }
+    })
 
-    }) as SearchResult
-    if (response.list.length > 0) {
-        response.list[3]
+    const result: Item[] = response.list?.map((i: { common: { title: string; description: string; thumbnailUrl: string[] } }) => {
+        return {
+            title: i.common.title,
+            description: i.common.description,
+            show: false,
+            fake: false,
+            thumb: i.common.thumbnailUrl ? i.common.thumbnailUrl[0] : ""
+        }
+    })
 
-        const desc = await genGpt("Q:" + response.list[3].common.title + "の特徴は？")
-        response.list[3].common.description = desc.replace(/A:/g, "")
+    try {
+        if (body.fake === "シンプル") {
+            const i = Math.floor(Math.random() * result.length);
+            result[i].description = await genGpt(result[i].title + "の特徴を500文字で説明してください")
+            result[i].description = result[i].description.replace(/A:/, "")
+            result[i].fake = true
+        } else if (body.fake === "トンデモ") {
+            const i = Math.floor(Math.random() * result.length);
+            //説明
+            result[i].description = await genGpt("次の説明文を、必ず「超大陸の古代文明」と「宇宙人」そして「秘密結社」が起源であるとする話で終わるように、500字で書き換えてください。" + result[i].description);
+
+            //画像
+            const res: any = await $fetch("/predictions", {
+                baseURL: config.public.replicateApiURL,
+                method: "POST",
+                headers: {
+                    Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    // Pinned to a specific version of instruct-pix2pix
+                    // See https://replicate.com/timothybrooks/instruct-pix2pix/versions
+                    version: "30c1d0b916a6f8efce20493f5d61ee27491ab2a60437c13c588468b9810ec23f",
+
+                    // This is the text prompt that will be submitted by a form on the frontend
+                    input: { prompt: "make it into cyborg", image: result[i].thumb },
+                }),
+            })
+            console.info(res);
+            if (res.output.length > 0) {
+                result[i].thumb = res.output[res.output.length - 1]
+            }
+
+            result[i].fake = true
+        }
+    } catch (e) {
+        console.error(e)
     }
 
-    return response
+    return result;
 })
