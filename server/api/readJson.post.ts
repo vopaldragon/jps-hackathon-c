b5a6dcc08d938c151d7c3ccf78146dae3adcf07d
Kazuki Imamura <kaizumaki@gmail.com>
2023-02-19 13:35:00 +0900

    jsonをspreadsheetから取得するように変更

--- server/api/readJson.post.ts
+++ server/api/readJson.post.ts
@@ -1,9 +1,26 @@
+import {Item} from '~/server/api/search'
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-  const json: any = await import('~/assets/fake.json')
-  const data: any[] = json.default
-  const filteredData = data.filter((item: any) => {
-    return item['キーワード'] === body.keyword
+  const response: any = await $fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1ydNpNYQ1g8KAoUWUoQ_6YvgPYEk5k08XnUleNsH-n3I/values/%E6%95%B4%E5%BD%A2%E3%83%87%E3%83%BC%E3%82%BF?key=AIzaSyDNPQiu_z4xWcVUZPVRVrGhpTYMd5pFEns`,
+  )
+  const keys = response.values.splice(0, 1)[0];
+  const jsonData = response.values.map(function(row: any[]) {
+    var obj = {}
+    row.map(function(item, index) {
+      obj[keys[index]] = item;
+    });
+    return obj;
+  });
+  const result: Item[] = jsonData.filter((item: any) => item['キーワード'] === body.keyword).map((i: { common: { title: string; description: string; thumbnailUrl: string[] } }) => {
+    return {
+      title: '',
+      description: i['フェイク解説'] ?? '',
+      show: false,
+      fake: false,
+      thumb: ""
+    }
   })
-  return filteredData;
+  return result
 })
